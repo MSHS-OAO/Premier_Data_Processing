9af88d7a50a5f4d00a52c0fdbdfe8965e17381db
Matthew Miesle <matthew.miesle@mssm.edu>
2022-09-01 17:12:42 -0400

    file clean up

--- Volume/MSBIB/MSDUS_Visits_new_dev.R
+++ Volume/MSBIB/MSDUS_Visits_new_dev.R
@@ -1,67 +1,17 @@
-# GENERAL REMINDERS -------------------------------------------------------
-
-# 1. The outline can be customized based on project requirements.
-# 2. Sections may be added, removed, reordered, renamed, etc. as needed.
-# 3. Reference checklist for commenting and style guidelines
-# 4. Create Sections using the menu Code > Insert Section
-#    or the shortcut Ctrl+Shift+R
-# 5. Create subsections by starting a Section with two # signs (e.g. ##).  
-#    - See example within this section.
-
-
-## subsection example ----------------------------------------------------
-# The header for this section is an example of how to indent subsections by
-# adding an additional # symbol.
-
-
 # Libraries ---------------------------------------------------------------
-# Include all the packages that will be used throughout the code.
-# This is where packages can be installed if the user does not have them
-# currently installed.
-
-# Ensuring the appropriate package versions are used for the project based on
-# renv usage
-# renv::restore()
-
-# Common Packages
-# library(here)
-# library(rmarkdown)
-# library(shiny)
-
-# tidyverse includes: dplyr, ggplot2, lubridate, purrr, readr, readxl,
-# reprex, stringr, tidyr...and more
-# See this link for full list: https://tidyverse.tidyverse.org/
-# library(tidyverse)
 
 library(readxl)
 library(xlsx)
 library(dplyr)
 
 
-# Source Global Functions -------------------------------------------------
-# Source scripts that house global functions used within this script.
-# Global functions would include functions that are used regularly
-# throughout the rest of the script and should make code more readable.
-
-# source()
-
-
 # Assigning Directory(ies) ------------------------------------------------
-# Define variables for frequently used root directories or full directories.
-# (This section could be combined with the Constants section.)
-# Reminder: avoid using setwd()
 
 ## Shared Drive Path (Generic) --------------------------------------------
 j_drive <- paste0("//researchsan02b/shr2/deans/Presidents")
 
-# Constants ---------------------------------------------------------------
-# Define constants that will be used throughout the code. These are the
-# variables that are calculated here and not changed in the rest of the code.
 
 # Data References ---------------------------------------------------------
-# (aka Mapping Tables)
-# Files that need to be imported for mappings and look-up tables.
-# (This section may be combined into the Data Import section.)
 
 ## Pay cycles -------------------------------------------------------------
 dict_pay_cycles <- read_xlsx(
@@ -79,6 +29,7 @@ dict_pay_cycles <- dict_pay_cycles %>%
   mutate(DATE = format(as.Date(DATE), "%m/%d/%Y"),
          START.DATE = format(as.Date(START.DATE), "%m/%d/%Y"),
          END.DATE = format(as.Date(END.DATE), "%m/%d/%Y"))
+
 
 ## Dictionary ------------------------------------------------------------
 
@@ -100,6 +51,7 @@ dict_epic <- read_xlsx(
 dict_epic_short <- dict_epic %>%
   select(`Epic Department Name`, `Volume ID`, `Cost Center`, `volume ratio`)
 
+
 ### rehab offsite ---------------------------------------------------------
 
 dict_rehab_docs <- read_xlsx(
@@ -108,12 +60,14 @@ dict_rehab_docs <- read_xlsx(
   skip = 0
 )
 
+
 # Data Import -------------------------------------------------------------
 
 path_data_epic <- choose.files(default = j_drive,
                                caption = "Select Epic file",
                                multi = F
                                )
+
 
 ## Skip rows and import ---------------------------------------------------
 
@@ -126,7 +80,7 @@ skip_ct_max <- 5
 
 while (d_check != 0 & skip_ct < skip_ct_max) {
   col_check <- read_xlsx(path_data_epic, sheet = 1, skip = skip_ct, n_max = 10)
-  
+
   if ("Department" %in% colnames(col_check)) {
     data_raw <- read_xlsx(path_data_epic, sheet = 1, skip = skip_ct)
     d_check <- 0
@@ -143,21 +97,7 @@ if (skip_ct == skip_ct_max) {
 }
 
 
-# Creation of Functions --------------------------------------------------
-# These are functions that will be commonly used within the rest of the script.
-# It might make sense to keep these files in a separate file that is sourced
-# in the "Source Global Functions" section above.
-
-
 # Data Pre-processing -----------------------------------------------------
-# Cleaning raw data and ensuring that all values are accounted for such as
-# blanks and NA. As well as excluding data that may not be used or needed. This
-# section can be split into multiple ones based on the data pre-processing
-# needed.
-# One of the first steps could be to perform initial checks to make sure data is
-# in the correct format.  This might also be done as soon as the data is
-# imported.
-
 
 data_epic <- data_raw %>%
   select(Department, Provider, `Appt Time`, `Epic Department ID`)
@@ -196,6 +136,7 @@ if (date_ok == "NO") {
 data_epic <- data_epic %>%
   left_join(dict_pay_cycles, by = c("Appt Date" = "DATE"))
 
+
 ## Join Dept & Vol ID -----------------------------------------------------
 
 data_epic_row <- nrow(data_epic)
@@ -225,6 +166,7 @@ data_epic <- data_epic %>%
     TRUE ~ volume
   ))
 
+
 ## upload summary ---------------------------------------------------------
 summary_upload <- data_epic %>%
   group_by(`Cost Center`, START.DATE, END.DATE, `Volume ID`) %>%
@@ -233,6 +175,7 @@ summary_upload <- data_epic %>%
   ungroup() %>%
   filter(!is.na(`Volume ID`)) %>%
   filter(!(`Volume ID` %in% c("TBD", "X")))
+
 
 ## Add rows for volume ID with 0 volume ------------------------------------
 
@@ -253,15 +196,22 @@ zero_rows <- missing_vol_id_date %>%
   mutate(visits = 0) %>%
   relocate(`Cost Center`, .before = START.DATE)
 
+zero_depts <- dict_epic_short %>%
+  filter(`Volume ID` %in% zero_rows$`Volume ID`)
+
 # would be better to display Cost Center name and the volume IDs
-if (length(unique(zero_rows$`Volume ID`)) > 0) {
-  winDialog(message = paste0("These volume IDs had a 0 volume:\r",
-                             paste(unique(zero_rows$`Volume ID`),
-                                   collapse = "; ")))
+if (length(unique(zero_depts$`Epic Department Name`)) > 0) {
+  winDialog(message = paste0(
+    "These Departments had a pay period with 0 volume:\r\r",
+    paste(sort(unique(zero_depts$`Epic Department Name`)),
+          collapse = "\n\n")),
+    type = "ok")
 }
+
 
 ### Combining with upload summary --------------------------------------------
 summary_upload <- rbind(summary_upload, zero_rows)
+
 
 # Data Formatting ---------------------------------------------------------
 # How the data will look during the output of the script.
@@ -274,23 +224,33 @@ upload_file <- summary_upload %>%
          budget = "0") %>%
   relocate(c(entity, facility), .before = `Cost Center`)
 
+
 # Quality Checks ----------------------------------------------------------
-# Checks that are performed on the output to confirm data consistency and
-# expected outputs.
 
 ## new departments --------------------------------------------------------
 
+new_dept <- data_epic %>%
+  filter(is.na(volume)) %>%
+  select(Department, `Epic Department ID`) %>%
+  unique() # %>%
 
+if (length(new_dept$Department) > 0) {
+  new_dept_stop <- winDialog(
+    message = paste0("These Departments are not in the dictionary:\r",
+                     paste(unique(zero_rows$`Volume ID`), collapse = "; "),
+                     "\r",
+                     "To stop running this script, press \"Cancel\" \r",
+                     "Press OK to continue"),
+    type = "okcancel")
+} else {
+  message("No new departments identified.")
+}
 
-## volume history ---------------------------------------------------------
-
-
-
-
-# Visualization -----------------------------------------------------------
-# How the data will be plotted or how the data table will look including axis
-# titles, scales, and color schemes of graphs or data tables.
-# (This section may be combined with the Data Formatting section.)
+if (new_dept_stop == "CANCEL") {
+  stop(paste0("Fix the dept dictionary based on the new departments.\n",
+              "Identify the appropriate file and restart")
+       )
+}
 
 
 # File Saving -------------------------------------------------------------
@@ -299,17 +259,17 @@ upload_file <- summary_upload %>%
 date_min_char <- format(as.Date(data_date_min, "%m/%d/%Y"), "%Y-%m-%d")
 date_max_char <- format(as.Date(data_date_max, "%m/%d/%Y"), "%Y-%m-%d")
 
-file_name_Premier <-
+file_name_premier <-
   paste0("MSDUS_Department Volumes_", date_min_char, "_to_", date_max_char,
          ".csv")
-path_folder_Premier_export <-
+path_folder_premier_export <-
   choose.dir(
     default = j_drive,
     caption = "Select folder to export Premier upload file"
   )
 write.table(
   upload_file,
-  file = paste0(path_folder_Premier_export, "\\", file_name_Premier),
+  file = paste0(path_folder_premier_export, "\\", file_name_premier),
   row.names = F,
   col.names = F,
   sep = ","
